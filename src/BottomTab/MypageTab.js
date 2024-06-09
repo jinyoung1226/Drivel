094f90bc943609f35f9e9a974e8a4dc1a33e120f
hwansoo17 <playground99@naver.com>
2024-06-10 02:04:54 +0900

    feat: 프로필 설정 화면 추가 및 로그아웃 버튼 마이페이지로 이동 - 프로필 설정 화면 추가 - 로그아웃 버튼을 마이페이지로 이동 - 사용자 경험 개선을 위한 UI 업데이트

--- src/BottomTab/MypageTab.js
+++ src/BottomTab/MypageTab.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import MyPageMain from '../Screens/Mypage/MypageMain';
-
+import ProfileSetting from '../Screens/Mypage/ProfileSetting';
 const Stack = createStackNavigator();
 
 const MypageTab = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Mypage" component={MyPageMain} />
+      <Stack.Screen name="ProfileSetting" component={ProfileSetting} />
     </Stack.Navigator>
   );
 };
