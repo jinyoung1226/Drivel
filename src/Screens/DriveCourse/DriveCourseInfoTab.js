7f9c480ca942d906d4c66270439c0978221674c7
Jin young IM <74377164+jinyoung1226@users.noreply.github.com>
2024-07-01 14:54:28 +0900

    코스디테일 일부 완료

---
+++ src/Screens/DriveCourse/DriveCourseInfoTab.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {textStyles} from '../../styles/textStyles';
+import colors from '../../styles/colors';
+
+const DriveCourseInfoTab = () => {
+  const tabName = ['상세정보', '리뷰', '관광지'];
+  const [activeTab, setActiveTab] = useState(0);
+  return (
+    <View style={styles.tabContainer}>
+      {tabName.map((item, index) => (
+        <TouchableOpacity
+          onPress={() => setActiveTab(index)}
+          style={styles.tabButton}>
+          <Text
+            style={[
+              styles.tabButtonText,
+              activeTab === index && styles.activeTabButton,
+            ]}>
+            {item}
+          </Text>
+          {activeTab === index && <View style={styles.activeTabUnderline} />}
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabContainer: {
+    height: 36,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 24,
+    paddingHorizontal: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.Gray04,
+  },
+
+  tabButton: {
+    flex: 1,
+    alignItems: 'center',
+  },
+
+  tabButtonText: [textStyles.H4, {color: colors.Gray04}],
+  activeTabButton: {
+    color: colors.Blue,
+  },
+  activeTabUnderline: {
+    position: 'absolute',
+    bottom: -1,
+    left: 0,
+    right: 0,
+    height: 2,
+    backgroundColor: '#0000FF', // 변경된 색상
+  },
+});
+
+export default DriveCourseInfoTab;
--- src/Screens/DriveCourse/DriveDetail.js
+++ src/Screens/DriveCourse/DriveDetail.js
@@ -1,18 +1,42 @@
 import {React, useEffect} from 'react';
 import {View, Text, Button, Image, StyleSheet, Dimensions} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
-
+import {textStyles} from '../../styles/textStyles';
+import colors from '../../styles/colors';
+import {ScrollView} from 'react-native-gesture-handler';
+import DriveCourseInfoTab from './DriveCourseInfoTab';
 const DriveDetail = () => {
   const driveInfo = useSelector(state => state.drive.driveInfo);
+  console.log(driveInfo);
+  const theme = ['노을 맛집', '해변길', '타다끼', '김수환 성적 a3,c3 ㅋㅋ'];
+
   const {width} = Dimensions.get('window');
 
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Image
-        source={require('../../assets/image/homeTopImg.jpg')}
+        source={(source = {url: driveInfo.courseInfo.imagePath})}
         style={[styles.image, {width: width}]}
       />
-    </View>
+      <View style={styles.tagContainer}>
+        {theme.map((item, index) => (
+          <View style={styles.tagButton}>
+            <Text style={styles.tagText}>{item}</Text>
+          </View>
+        ))}
+      </View>
+      <View style={{paddingHorizontal: 16, marginTop: 16}}>
+        <Text style={[textStyles.H1, {color: colors.Gray10}]}>
+          {driveInfo.courseInfo.name}
+        </Text>
+        <View style={{height: 8}} />
+        <Text style={[textStyles.M14, {color: colors.Gray07}]}>
+          {driveInfo.courseInfo.description}
+        </Text>
+      </View>
+      <View style={styles.bar}></View>
+      <DriveCourseInfoTab />
+    </ScrollView>
   );
 };
 
@@ -21,8 +45,35 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#ffffff',
   },
+
   image: {
-    height: 210,
+    height: 210.94,
+  },
+
+  tagContainer: {
+    marginTop: 16.06,
+    paddingHorizontal: 16,
+    height: 30,
+    flexDirection: 'row',
+    justifyContent: 'left',
+    gap: 4,
+  },
+
+  tagButton: {
+    paddingHorizontal: 10,
+    height: 30,
+    backgroundColor: '#E3E7FF',
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  tagText: [textStyles.B2, {color: colors.Blue}],
+
+  bar: {
+    height: 10,
+    marginTop: 17,
+    backgroundColor: '#F6F6F7',
   },
 });
 
