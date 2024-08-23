39c6b50ffd7e87b5cc3e390bd99ed28f9a41730e
suhwan kim <playground99@naver.com>
2024-08-23 22:16:27 +0900

    feat: 도움말 제공 기능 구현

--- src/Screens/Mypage/Help.js
+++ src/Screens/Mypage/Help.js
@@ -1,32 +1,52 @@
-import React, {useLayoutEffect} from 'react';
-import {Text, View, TouchableOpacity, Linking} from 'react-native';
+import React, {useLayoutEffect, useState} from 'react';
+import {Text, View, TouchableOpacity} from 'react-native';
 import {textStyles} from '../../styles/textStyles';
 import colors from '../../styles/colors';
-import { FlatList, ScrollView } from 'react-native-gesture-handler';
+import { FlatList } from 'react-native-gesture-handler';
 import BackIcon from '../../assets/icons/BackIcon';
-import {useDispatch} from 'react-redux';
-import {logout} from '../../features/auth/authActions';
-import { helps } from '../../assets/helpData/helpData';
 import ArrowIcon from '../../assets/icons/ArrowIcon';
+import { helps } from '../../assets/helpData/helpData';
 
-const Help = ({navigation, route}) => {
+const Help = ({navigation}) => {
 
-  const HelpListItem = ({title, onPress}) => {
+  const [selectedItems, setSelectedItems] = useState([]); 
+
+  const HelpListItem = ({item, onPress, id}) => {
+    const isSelected = selectedItems.includes(id);
+
     return (
-      <TouchableOpacity 
-        onPress={onPress}
-        style={{flex:1, paddingHorizontal:16, paddingVertical:20, borderColor: colors.Gray03, borderRadius:10, borderWidth:1, marginHorizontal:16, marginBottom:16, flexDirection:'row'}}>
-        <View style={{flex:1}}>
-          <Text style={[textStyles.H5, {color:colors.Gray10}]}>{title}</Text>
-        </View>
-        <View>
-          <ArrowIcon width={24} height={24} transform={[{rotate: '90deg'}]}/>
-        </View>
-      </TouchableOpacity>
-    ) 
+      <View style={{flex:1, paddingHorizontal:16, borderColor: colors.Gray03, borderRadius:10, borderWidth:1, marginHorizontal:16, marginBottom:16}}>
+        <TouchableOpacity 
+          onPress={onPress}
+          style={{flexDirection:'row', alignItems:'center', paddingVertical:20}}
+          >
+          <View style={{flex:1}}>
+            <Text style={[textStyles.H5, {color:colors.Gray10}]}>{item.title}</Text>
+          </View>
+          <View style={{width:16}}/>
+          <View>
+            <ArrowIcon width={24} height={24} transform={[{rotate: isSelected ? '-90deg' : '90deg'}]}/>
+          </View>
+        </TouchableOpacity>
+        {isSelected && (
+          <View>
+            <View style={{height:1, backgroundColor:colors.Gray03}}/>
+            <View style={{paddingVertical:16}}>
+              <Text style={[textStyles.B4, {color:colors.Gray07}]}>{item.content}</Text>
+            </View>
+          </View>
+        )}
+      </View>
+    ); 
   }
   
-  const dispatch = useDispatch();
+  const toggleItem = (id) => {
+    if (selectedItems.includes(id)) {
+      setSelectedItems(selectedItems.filter(itemId => itemId !== id));
+    } else {
+      setSelectedItems([...selectedItems, id]);
+    }
+  }
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -46,16 +66,15 @@ const Help = ({navigation, route}) => {
   }, [navigation]);
 
   return(
-    
-      <View style={{backgroundColor:colors.BG, flex:1}}>
-        <FlatList
-          data={helps}
-          renderItem={({item}) => (
-            <HelpListItem title={item.title} onPress={{}}/>
-          )}
-          keyExtractor={(item) => item.id}
-        />
-      </View>
+    <View style={{backgroundColor:colors.BG, flex:1}}>
+      <FlatList
+        data={helps}
+        renderItem={({item}) => (
+          <HelpListItem item={item} onPress={() => {toggleItem(item.id)}} id={item.id}/>
+        )}
+        keyExtractor={(item) => item.id.toString()}
+      />
+    </View>
   )
 }
 
